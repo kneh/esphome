a163f74cbc583a218036940cba106f6520003dcc
kneh <henk.stegeman@gmail.com>
2023-10-06 22:28:43 +0200

    Update ili9xxx_display.cpp
    Typo

--- esphome/components/ili9xxx/ili9xxx_display.cpp
+++ esphome/components/ili9xxx/ili9xxx_display.cpp
@@ -45,7 +45,7 @@ void ILI9XXXDisplay::setup_pins_() {
 
 void ILI9XXXDisplay::dump_config() {
   LOG_DISPLAY("", "ili9xxx", this);
-  ESP_LOGCONFIG(TAG, "  ID: 0x%08lx",this->_id_lcd());
+  ESP_LOGCONFIG(TAG, "  ID: 0x%08lx",this->id_lcd_());
   switch (this->buffer_color_mode_) {
     case BITS_8_INDEXED:
       ESP_LOGCONFIG(TAG, "  Color mode: 8bit Indexed");
